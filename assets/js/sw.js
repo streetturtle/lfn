968a6a0e0d90f2d44982a8ce2d15734e2b6d126b
Pavel Makhov <pmakhov@touchtunes.com>
2019-01-02 20:36:44 -0500

    sw

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -13,11 +13,29 @@ const PRECACHE_URLS = [
 ];
 
 // The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
+// self.addEventListener('install', event => {
+//     event.waitUntil(
+//         caches.open(PRECACHE)
+//             .then(cache => cache.addAll(PRECACHE_URLS))
+//             .then(self.skipWaiting())
+//     );
+// });
+
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(PRECACHE)
-            .then(cache => cache.addAll(PRECACHE_URLS))
-            .then(self.skipWaiting())
+        caches.open(PRECACHE).then(function(cache) {
+            return cache.addAll(
+                [
+                    '/lfn/#',
+                    '/lfn/index.html',
+                    '/lfn/assets/css/materialize.css',
+                    '/lfn/assets/css/app.css',
+                    '/lfn/assets/js/app.js',
+                    '/lfn/assets/js/written-number.min.js',
+                    '/lfn/assets/js/materialize.min.js',
+                ]
+            );
+        })
     );
 });
 
@@ -76,26 +94,41 @@ self.addEventListener('activate', event => {
 // });
 
 
-self.addEventListener('fetch', event => {
-    // We only want to call event.respondWith() if this is a navigation request
-    // for an HTML page.
-    // request.mode of 'navigate' is unfortunately not supported in Chrome
-    // versions older than 49, so we need to include a less precise fallback,
-    // which checks for a GET request with an Accept: text/html header.
-    if (event.request.mode === 'navigate' ||
-        (event.request.method === 'GET' &&
-            event.request.headers.get('accept').includes('text/html'))) {
-        console.log('Handling fetch event for', event.request.url);
-        event.respondWith(
-            fetch(event.request).catch(error => {
-                // The catch is only triggered if fetch() throws an exception, which will most likely
-                // happen due to the server being unreachable.
-                // If fetch() returns a valid HTTP response with an response code in the 4xx or 5xx
-                // range, the catch() will NOT be called. If you need custom handling for 4xx or 5xx
-                // errors, see https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/fallback-response
-                console.log('Fetch failed; returning offline page instead.', error);
-                return caches.match(event.request);
-            })
-        );
-    }
+// self.addEventListener('fetch', event => {
+//     // We only want to call event.respondWith() if this is a navigation request
+//     // for an HTML page.
+//     // request.mode of 'navigate' is unfortunately not supported in Chrome
+//     // versions older than 49, so we need to include a less precise fallback,
+//     // which checks for a GET request with an Accept: text/html header.
+//     if (event.request.mode === 'navigate' ||
+//         (event.request.method === 'GET' &&
+//             event.request.headers.get('accept').includes('text/html'))) {
+//         console.log('Handling fetch event for', event.request.url);
+//         event.respondWith(
+//             fetch(event.request).catch(error => {
+//                 // The catch is only triggered if fetch() throws an exception, which will most likely
+//                 // happen due to the server being unreachable.
+//                 // If fetch() returns a valid HTTP response with an response code in the 4xx or 5xx
+//                 // range, the catch() will NOT be called. If you need custom handling for 4xx or 5xx
+//                 // errors, see https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/fallback-response
+//                 console.log('Fetch failed; returning offline page instead.', error);
+//                 return caches.match(event.request);
+//             })
+//         );
+//     }
+// });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request));
+
+    // event.respondWith(
+    //     caches.open('mysite-dynamic').then(function(cache) {
+    //         return cache.match(event.request).then(function (response) {
+    //             return response || fetch(event.request).then(function(response) {
+    //                 cache.put(event.request, response.clone());
+    //                 return response;
+    //             });
+    //         });
+    //     })
+    // );
 });
